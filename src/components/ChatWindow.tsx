0d336a517f92c1f988aa52d40214d003cbd226aa
CharanHC <152987714+CharanHC@users.noreply.github.com>
2025-08-11 23:10:52 +0530

    Update ChatWindow.tsx

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -36,9 +36,9 @@ export default function ChatWindow({ waId, name, api }: ChatWindowProps) {
     return () => clearInterval(intervalId);
   }, [waId, api]);
 
-  useEffect(() => {
-    chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [messages]);
+  //useEffect(() => {
+    //chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  //}, [messages]);
 
   const deleteMessage = (id: string) => {
     setMessages((prev) => prev.filter((m) => m._id !== id));
